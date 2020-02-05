7bc37226c56910735f6e83c978c16300c875726d
peterglezroa <peterglezroa@gmail.com>
2020-02-05 14:48:28 -0600

    On branch feature/configs  Summary:  * multiple monitors  Changes to be committed: 	modified:   bindsyms/actions.txt   modified:   polybar/config 	modified:   polybar/launch.sh 	modified:   scripts/.workflow_config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,7 +8,16 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 #echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar peterglezroa >>/tmp/polybarpeterglezroa.log 2>&1 &
-#polybar bar2 >>/tmp/polybar2.log 2>&1 &
-
-echo "Bar launched..."
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d' ' -f1); do
+    if [ "$m" = "HDMI-1" ] 
+    then
+      MONITOR=$m polybar --reload HDMI-1 &
+    else
+      MONITOR=$m polybar --reload peterglezroa &
+    fi
+  done
+else
+  polybar peterglezroa >>/tmp/polybarpeterglezroa.log 2>&1 &
+  echo "Bar launched..."
+fi
