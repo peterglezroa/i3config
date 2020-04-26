8ccd1978ff0c4147f4ffeb8440f4d4d2af55effa
peterglezroa <peterglezroa@gmail.com>
2020-04-26 15:57:03 -0500

    Terminal  On branch temporal  Changes to be committed: 	renamed:    ../build -> ../build.sh 	renamed:    build -> build.sh 	new file:   config/colorschemes/peterglezroa.json 	new file:   config/colorschemes/shell/peterglezroa.json 	modified:   config/config.json 	new file:   config/themes/shell/peterglezroa.json

--- terminal/build.sh
+++ terminal/build.sh
@@ -8,7 +8,9 @@
 
 # Dependencies
 sudo apt-get install neofetch -y
-sudo pip3 install powerline-status
+sudo pip3 install powerline-gitstatus
+
+# Variables
 POWERLINELOCATION=$(pip3 show powerline-status | grep Location | tr -s ' ' '#' | cut -f2 -d#)
 
 # Fonts
@@ -18,6 +20,7 @@ wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbol
 sudo mv PowerlineSymbols.otf ~/.fonts/
 
 fc-cache -vf ~/.fonts/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 
 # Copy config
 cp config ~/.config/powerline
@@ -31,3 +34,14 @@ echo ". $POWERLINELOCATION/powerline/bindings/bash/powerline.sh" >> ~/.bashrc
 #echo "python3 from powerline.vim import setup as powerline_setup" >> ~/.vimrc
 #echo "python3 powerline_setup()" >>  ~/.vimrc
 #echo "python3 del powerline_setup" >> ~/.vimrc
+
+
+
+# z script
+cd ~/tmp
+git clone https://github.com/rupa/z
+mv z/z.sh ~/bin
+echo ". \$HOME/z.sh" >> ~/.bashrc
+
+# Alias
+echo "alias venv=\"source .env/bin/activate\"" >> .profile
