e842e8822a9c1dca145e8423dc6d1678f4251cfd
peterglezroa <peterglezroa@gmail.com>
2020-04-26 17:37:06 -0500

    Compton  On branch temporal  Changes to be committed: 	deleted:    compton/build 	new file:   compton/build.sh

---
+++ compton/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install -y compton
+echo "opacity-rule = [\"85:class_g = \'gnome-terminal\'\"];" > ~/.config/compton.conf
+echo "for_window [class="^.*"] border pixel 2 #get rid of tittlebars because gaps doesnt work" >> ~/.config.compton.conf
